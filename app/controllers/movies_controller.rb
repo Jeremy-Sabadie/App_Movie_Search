f6d6fd7c49d27a7e7e64218d6611af39e849c203
Morpheus974 <sabadie.jeremy@gmail.com>
2018-02-15 22:05:06 +0100

    suppression .first(20)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,6 @@ class MoviesController < ApplicationController
   def search
   	@my_res = nil
   	@usersearch = params[:usersearch]
-    @my_res = SearchMovie.new(@usersearch).perform.first(20)
+    @my_res = SearchMovie.new(@usersearch).perform
   end
 end
